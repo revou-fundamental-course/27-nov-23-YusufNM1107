07ccef1bf3279303f3d2b10c0e3d63bef45b9d7c
unknown <runruntemboro123@gmail.com>
2023-12-06 23:02:29 +0800

    first push

---
+++ JS/Script.js
@@ -0,0 +1,22 @@
+function luasSegitiga() {
+    let alas = document.getElementById("input-alas").value;
+    let tinggi = document.getElementById("input-tinggi").value;
+    let hasilLuas =  parseInt(alas) * parseInt(tinggi) * 0.5
+    const keteranganLuas = "Luas Segitiga di atas adalah "
+
+    document.getElementById("hasilLuas").innerHTML = keteranganLuas + hasilLuas ;
+    }
+
+
+
+function kelilingsegitiga() {
+    let sisi1 = document.getElementById("input-sisi1").value;
+    let sisi2 = document.getElementById("input-sisi2").value;
+    let sisi3 = document.getElementById("input-sisi3").value;
+    const keteranganKeliling = "Keliling Segitiga di atas adalah "
+    let hasilKeliling = parseInt(sisi1) + parseInt(sisi2) + parseInt(sisi3)
+
+    document.getElementById("hasilKeliling").innerHTML = keteranganKeliling + hasilKeliling ;
+    
+}
+
